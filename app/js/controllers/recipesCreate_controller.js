a738e6b4953536cd6fbfd43cf72bfaf415615568
Patrick Love <plove13x@gmail.com>
2014-10-16 18:04:10 -0400

    Fixes async controversy when creating new recipes

---
+++ app/js/controllers/recipesCreate_controller.js
@@ -0,0 +1,36 @@
+BM.RecipesCreateController = Ember.Controller.extend({
+	
+	needs: 'application',
+	recipeName: '',
+	recipeDetails: '',
+	user: Ember.computed.alias('controllers.application.user'),
+	init: function() {
+		console.log(this.get('user'));
+		console.log(this.get('controllers.application.user.data'));
+	},
+
+	actions: {
+		createRecipe: function() {
+			var user = this.get('user');
+			// this.store.createRecord('user');
+			console.log(user);
+			var recipe = this.store.createRecord('recipe', {
+				name: this.get('recipeName'),
+				recipeDetails: this.get('recipeDetails'),
+				user: user
+			});
+			recipe.save().then(function(){
+				user.get('recipes').then(function(f){
+					f.addObject(recipe);
+					user.save();
+				});
+			});
+			// user.get('recipes').addObject(recipe);
+			this.set('recipeName', '');
+			this.set('recipeDetails', '');
+		
+
+		}
+	},
+
+});
